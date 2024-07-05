82f1361583b8ff55aa0a0c770a7b09c186ccbc04
Bart <liempdbart@gmail.com>
2024-07-05 20:56:24 +0200

    Added dynamic route support

--- script.js
+++ script.js
@@ -7,10 +7,50 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
-const kerryWay = omnivore.kml("hikes\\kerryWay.kml").addTo(map);
-const balkanPeaks = omnivore.kml("hikes\\balkanPeaks.kml").addTo(map);
+const hikeNames =["balkanPeaks","kerryWay"]
+
+function formatName(name){
+    name.split("").forEach((char) => {
+        if (char === char.toUpperCase()){
+            const indexUpper = name.indexOf(char)
+            name = `${name[0].toUpperCase()}${name.slice(1,indexUpper)} ${name.slice(indexUpper)}`;     
+        }
+    })
+    return name;
+}
 
 
+function addLocation(name){
+    const location = omnivore.kml(`hikes\\${name}.kml`).addTo(map);
+    return location
+}
+
+
+
+/*Inputs addButton:
+    - name: string
+    - htmlElement: string
+    - location: leaflet location
+*/
+function addButton(name, htmlElement, location){
+    const button = document.createElement("button");
+    button.type = "button";
+    button.id = "name";
+    button.className = htmlElement;
+    button.onclick = function() {zoomToLocation(location);};
+    button.innerText = formatName(name);
+    buttons.append(button);
+    button.style.backgroundImage=`url("fotos/${name}.jpeg")`
+}
+
+
+hikeNames.forEach((hike) => {
+    addButton(hike, "sidebar_button", addLocation(hike))
+    
+})
+
+
+    
 
 
 const height = window.screen.height
--- zoomToLocation.js
+++ zoomToLocation.js
@@ -1,7 +1,5 @@
-
 function zoomToLocation(location){
     const bounds = location.getBounds()
-    console.log(bounds)
     map.fitBounds(bounds, {padding: [200,200]});
 }
     
