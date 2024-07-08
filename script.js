7745e1fb20e22bb98386070e5012d15976c4ab87
Bart <liempdbart@gmail.com>
2024-07-08 22:38:33 +0200

    Dynamic initial extent, however still bugged with duplicates

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+"use strict";
+// Andere manier vinden om de promises te gebruiken
+
+
+
+/* 
+Define the map and the view of the map
+*/
+
 const map = L.map('map').setView([51.505, -0.09], 13);
 
+/* 
+Add the tilelayer of openstreetmap to the map
+*/
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
+/*
+Define the name of the hikes
+*/
 
 const hikeNames =["balkanPeaks","kerryWay"]
+let arrayLocation =[]
+
+
+function createInitialExtent(arr){
+    return Promise.all(hikeNames.map(hike => addLocation(hike, arrayLocation)))
+    .then(arr => {
+        if (arr.length === 0){
+        console.log("createInitialExtent inputarray length is 0")
+        }
+        else if (arr.length === 1) {
+            return(arr[0])
+        }
+        else if (arr.length > 1) {
+            let locationString = arr[0].getBounds()
+            arr.forEach((location, i) => {
+                if (i > 0){
+                    locationString = locationString.extend(arr[i].getBounds())
+                }
+                })
+            return locationString
+        }
+        
+    })
+}
+
+
+/*
+Format the name of the hike to nice format
+Example: balkanPeaks -> Balkan Peaks
+*/
 
 function formatName(name){
     name.split("").forEach((char) => {
@@ -19,15 +64,24 @@ function formatName(name){
     return name;
 }
 
+/*
+Add kml file to the map
+*/
 
-function addLocation(name){
-    const location = omnivore.kml(`hikes\\${name}.kml`).addTo(map);
-    return location
+function addLocation(name, array) {
+    return new Promise((resolve, reject) => {
+        const location = omnivore.kml(`hikes/${name}.kml`).addTo(map);
+        location.on('ready', function() {
+            const bounds = location.getBounds();
+            array.push(bounds);
+            resolve(location); // Resolve the promise with the location object
+        });
+    });
 }
 
 
-
-/*Inputs addButton:
+/*
+Inputs addButton:
     - name: string
     - htmlElement: string
     - location: leaflet location
@@ -35,23 +89,43 @@ function addLocation(name){
 function addButton(name, htmlElement, location){
     const button = document.createElement("button");
     button.type = "button";
-    button.id = "name";
+    button.id = name;
     button.className = htmlElement;
-    button.onclick = function() {zoomToLocation(location);};
+    button.onclick = function() {zoomToLocation(location)};
     button.innerText = formatName(name);
     buttons.append(button);
     button.style.backgroundImage=`url("fotos/${name}.jpeg")`
 }
 
 
+/*
+Loop throught the hikenames and add a button and location for each hike
+Used in addButton function
+*/
 hikeNames.forEach((hike) => {
-    addButton(hike, "sidebar_button", addLocation(hike))
-    
+    const locationPromise = addLocation(hike, arrayLocation);
+    addButton(hike, "sidebar_button", locationPromise)
 })
 
 
+Promise.all(hikeNames.map(hike => addLocation(hike, arrayLocation)))
+    .then(locations => {
+        // Perform map fitting once all locations are loaded
+        createInitialExtent().then(value => {map.fitBounds(value)})
+    })
+    .catch(error => {
+        console.error('Error loading locations:', error);
+    });
+
+
     
+/*
+Get the height of the screen
+NOT USED YET
+*/
+
 
 
 const height = window.screen.height
 console.log(height)
+
--- zoomToLocation.js
+++ zoomToLocation.js
@@ -1,5 +1,17 @@
-function zoomToLocation(location){
+/*function zoomToLocation(location){
+    console.log(location)
     const bounds = location.getBounds()
+    console.log(bounds)
     map.fitBounds(bounds, {padding: [200,200]});
+}*/
+
+function zoomToLocation(locationPromise) {
+    locationPromise.then(location => {
+        const bounds = location.getBounds();
+        map.fitBounds(bounds, { padding: [200, 200] });
+    }).catch(error => {
+        console.error('Error zooming to location:', error);
+    });
 }
     
+
